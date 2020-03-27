01e16af05dc37dd9b422d59acd487c351f9df470
dj0nr1kk0 <djon.rikko@gmail.com>
2020-03-27 16:44:21 +0700

    gas
    Gas

---
+++ kasus.php
@@ -0,0 +1,275 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<!-- Mirrored from www.enableds.com/products/asterial/asterial-footer/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 26 Mar 2020 07:30:00 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<title>Kasus Covid Indonesia</title>
+
+<link rel="stylesheet" type="text/css" href="styles/framework.css">
+<link href="css/cssd412.css?family=IBM+Plex+Sans:300,300i,400,400i,500,500i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="fonts/css/fontawesome-all.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+
+
+<link rel="manifest" href="_manifest.json" data-pwa-version="set_by_pwa.html">
+<link rel="apple-touch-icon" sizes="180x180" href="app/icons/icon-192x192.png">
+
+
+<script src="https://www.gstatic.com/firebasejs/4.9.1/firebase.js"></script>
+<script src="js/jquery-3.2.1.min.js"></script>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+<link rel="manifest" href="manifest.json">
+<script>
+  // Initialize Firebase
+  /*Update this config*/
+	var config = {
+    apiKey: "AIzaSyDG1kUz4mb65YpxC1Un3c-SwkP5vbqpPvY",
+    authDomain: "e-document-6ebe9.firebaseapp.com",
+    databaseURL: "https://e-document-6ebe9.firebaseio.com",
+    projectId: "e-document-6ebe9",
+    storageBucket: "e-document-6ebe9.appspot.com",
+    messagingSenderId: "934218502137",
+    appId: "1:934218502137:web:aed8cad6af347cd69d7728"
+
+
+
+  };
+  firebase.initializeApp(config);
+
+	// Retrieve Firebase Messaging object.
+const messaging = firebase.messaging();
+	messaging.requestPermission()
+	.then(function() {
+	  console.log('Notification permission granted.');
+	  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+	  if(isTokenSentToServer()) {
+	  	console.log('Token already saved.');
+	  } else {
+	  	getRegToken();
+	  }
+
+	})
+	.catch(function(err) {
+	  console.log('Unable to get permission to notify.', err);
+	});
+
+	function getRegToken(argument) {
+		messaging.getToken()
+		  .then(function(currentToken) {
+		    if (currentToken) {
+		      saveToken(currentToken);
+		      console.log(currentToken);
+		      setTokenSentToServer(true);
+		    } else {
+		      console.log('No Instance ID token available. Request permission to generate one.');
+		      setTokenSentToServer(false);
+		    }
+		  })
+		  .catch(function(err) {
+		    console.log('An error occurred while retrieving token. ', err);
+		    setTokenSentToServer(false);
+		  });
+	}
+
+	function setTokenSentToServer(sent) {
+	    window.localStorage.setItem('sentToServer', sent ? 1 : 0);
+	}
+
+	function isTokenSentToServer() {
+	    return window.localStorage.getItem('sentToServer') == 1;
+	}
+
+	function saveToken(currentToken) {
+		$.ajax({
+			url: 'fcm-push/action.php',
+			method: 'post',
+			data: 'token=' + currentToken
+		}).done(function(result){
+			console.log(result);
+		})
+	}
+
+	messaging.onMessage(function(payload) {
+	  console.log("Message received. ", payload);
+	  notificationTitle = payload.data.title;
+	  notificationOptions = {
+	  	body: payload.data.body,
+	  	icon: payload.data.icon,
+	  	image:  payload.data.image
+	  };
+	  var notification = new Notification(notificationTitle,notificationOptions);
+	});
+</script>
+
+</head>
+
+
+
+<body class="theme-light" data-highlight="blue2">
+<div id="page">
+<div id="page-preloader">
+<div class="loader-main"><div class="preload-spinner border-highlight"></div></div>
+</div>
+
+
+<div class="header header-fixed header-logo-left header-auto-show">
+<a href="#" class="back-button header-title">Covid 19 ID</a>
+<a href="#" class="header-icon header-icon-1" data-toggle-theme><i class="fas fa-moon"></i></a>
+</div>
+
+
+<div class="footer-menu footer-3-icons footer-menu-center-icon">
+<a href="kasus.php" class="active-nav"><i class="fa fa-gift"></i><span>Kasus</span></a>
+<a href="index.php" ><i class="fa fa-home"></i><span>Home</span></a>
+<a href="update.php"><i class="fa fa-bars"></i><span>Update Harian</span></a>
+<div class="clear"></div>
+</div>
+
+
+
+
+<div class="page-content">
+	<div class="page-title">
+	<div class="page-title-left">
+	<a href="#">Covid 19 ID</a>
+	</div>
+	<div class="page-title-right">
+	<a href="#" data-toggle-theme><i class="fa fa-moon"></i></a>
+	</div>
+	</div>
+		
+		
+
+
+<div class="divider divider-margins"></div>
+<div class="content">
+<h2>Berdasrkan Kasus</h2>
+<p>
+Data Berdasarkan Yang Terjadi di Indonesia
+</p>
+<table class="table-borders shadow-huge round-medium">
+<tr>
+<td class="bg-dark2-dark"><h4>No</h4></td>
+<td class="bg-dark2-dark"><h4>Provinsi</h4></td>
+<td class="bg-dark2-dark"><h4>Jenis Kelamain/Umur</h4></td>
+<td class="bg-dark2-dark"><h4>Status</h4></td>
+<td class="bg-dark2-dark"><h4>Warga Negara</h4></td>
+
+</tr>
+<?php 
+								$no=0;
+								$url="https://indonesia-covid-19.mathdro.id/api/kasus";
+								$json = file_get_contents("$url");
+								$books = json_decode($json,true);
+								foreach ($books['data']['nodes'] as $bok){
+		
+								?>
+<tr>
+<td class="bg-dark1-light"><?php echo ++$no; ?></td>
+<td class="bg-dark1-light"><?php echo $bok['klaster']; ?></td>
+<td class="bg-dark1-light"><?php echo $bok['gender']; ?>/<?php echo $bok['umur']; ?></td>
+<td class="bg-dark1-light"><?php echo $bok['status']; ?></td>
+<td class="bg-dark1-light"><?php echo $bok['wn']; ?></td>
+
+</tr>
+
+								<?php } ?>
+
+
+</table>
+</div>
+
+
+
+
+
+
+
+
+<div class="divider bottom-20"></div>
+
+</div>
+<div class="menu-hider"></div>
+</div>
+
+
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/owl.carousel.min.js" type="text/javascript" charset="utf-8"></script>
+<script>
+  $('.owl-duo').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<span class='glyphicon glyphicon-chevron-left'></span>",
+    "<span class='glyphicon glyphicon-chevron-right'></span>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    200: {
+      items: 2
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+	items:2,
+    nav:false,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:false
+    }
+  }
+})
+</script>
+		
+<script>
+  $('.owl-trio').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<span class='glyphicon glyphicon-chevron-left'></span>",
+    "<span class='glyphicon glyphicon-chevron-right'></span>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    200: {
+      items: 3
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+	items:3,
+    nav:false,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:false
+    }
+  }
+})
+</script>
+
+
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.js"></script>
+<script type="text/javascript" src="scripts/plugins.js"></script>
+<script type="text/javascript" src="scripts/custom.js" async></script>
+
+</body>
